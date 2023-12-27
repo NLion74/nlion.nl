08c75aec15c37e88107625a9025756886944b8eb
NLion74 <nlion7444@gmail.com>
2023-12-27 18:00:13 +0100

    Updated ageUpdate to use german timezone

--- src/assets/js/ageUpdate.js
+++ src/assets/js/ageUpdate.js
@@ -8,7 +8,15 @@ function setAge(age_string) {
 
 function getAge() {
     const time =
-        (new Date().getTime() - new Date(2007, 9, 10).getTime()) /
+        (new Date(
+            new Date().toLocaleString({ timeZone: "Europe/Berlin" })
+        ).getTime() -
+            // Date 2007-09-09 is equal to 2007-10-9 because january is 0 and december 11
+            new Date(
+                new Date(2007, 9, 9, 0, 0, 0).toLocaleString({
+                    timeZone: "Europe/Berlin",
+                })
+            ).getTime()) /
         (1000 * 60 * 60 * 24 * 365.25); // milliseconds per year
     const age = time.toString().substring(0, 11);
 
